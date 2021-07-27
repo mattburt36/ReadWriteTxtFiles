ab9d750c0bfa13689a276931203fefb0546279a3
mattburt36 <matt-burton@live.nmit.ac.nz>
2021-07-27 19:47:29 +1200

    draggable, exit and minimalise buttons done

--- ReadWriteTxtFiles/MyForm.h
+++ ReadWriteTxtFiles/MyForm.h
@@ -9,24 +9,16 @@ namespace ReadWriteTxtFiles {
 	using namespace System::Data;
 	using namespace System::Drawing;
 
-	/// <summary>
-	/// Summary for MyForm
-	/// </summary>
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
 	public:
 		MyForm(void)
 		{
 			InitializeComponent();
-			//
-			//TODO: Add the constructor code here
-			//
 		}
 
 	protected:
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
+
 		~MyForm()
 		{
 			if (components)
@@ -34,26 +26,102 @@ namespace ReadWriteTxtFiles {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button1;
+	protected:
 
 	private:
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
+
 		System::ComponentModel::Container ^components;
 
 #pragma region Windows Form Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
+
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::Color::Black;
+			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button2->Font = (gcnew System::Drawing::Font(L"Segoe UI", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button2->ForeColor = System::Drawing::Color::Snow;
+			this->button2->Location = System::Drawing::Point(520, 311);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(67, 29);
+			this->button2->TabIndex = 11;
+			this->button2->Text = L"Exit";
+			this->button2->UseVisualStyleBackColor = false;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::Black;
+			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Segoe UI", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->ForeColor = System::Drawing::Color::Snow;
+			this->button1->Location = System::Drawing::Point(447, 311);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(67, 29);
+			this->button1->TabIndex = 12;
+			this->button1->Text = L"Minimise";
+			this->button1->UseVisualStyleBackColor = false;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->ClientSize = System::Drawing::Size(599, 352);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->button2);
+			this->DoubleBuffered = true;
+			this->Font = (gcnew System::Drawing::Font(L"Segoe UI", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
+			this->Name = L"MyForm";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
-			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->MouseDown += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::MyForm_MouseDown);
+			this->MouseMove += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::MyForm_MouseMove);
+			this->MouseUp += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::MyForm_MouseUp);
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
+
+
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) 
+	{
+		Application::Exit(); // When click exit button exit the application 
+	}
+
+		   bool dragging;
+		   Point offset;
+	private: System::Void MyForm_MouseDown(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e)
+	{
+		// Enable dragging and get mouse position
+		dragging = true;
+		offset.X = e->X;
+		offset.Y = e->Y;
+	}
+	private: System::Void MyForm_MouseMove(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e)
+	{
+		// Check whether able to move
+		if (dragging)
+		{
+			Point currentScreenPosition = PointToScreen(Point(e->X, e->Y));
+			Location = Point(currentScreenPosition.X - offset.X, currentScreenPosition.Y - offset.Y);
+		}
+	}
+	private: System::Void MyForm_MouseUp(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e)
+	{
+		// Disable dragging 
+		dragging = false;
+	}
 	};
 }
