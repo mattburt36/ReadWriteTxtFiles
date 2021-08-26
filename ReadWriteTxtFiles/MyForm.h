eeb60b45843d25388b3c4879bdf7541f3ae4064c
Matthew Burton <36slices@gmail.com>
2021-08-26 18:45:35 +1200

    Write, create files complete

--- ReadWriteTxtFiles/MyForm.h
+++ ReadWriteTxtFiles/MyForm.h
@@ -8,6 +8,7 @@ namespace ReadWriteTxtFiles {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace System::IO;
 
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
@@ -26,8 +27,18 @@ namespace ReadWriteTxtFiles {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Button^ button2;
-	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ exitBtn;
+	protected:
+
+	private: System::Windows::Forms::Button^ createTxtFileBtn;
+	private: System::Windows::Forms::TextBox^ inputTextBox;
+	private: System::Windows::Forms::TextBox^ namingTextBox;
+	protected:
+
+
+
+
+
 	protected:
 
 	private:
@@ -39,46 +50,67 @@ namespace ReadWriteTxtFiles {
 		void InitializeComponent(void)
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
-			this->button2 = (gcnew System::Windows::Forms::Button());
-			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->exitBtn = (gcnew System::Windows::Forms::Button());
+			this->createTxtFileBtn = (gcnew System::Windows::Forms::Button());
+			this->inputTextBox = (gcnew System::Windows::Forms::TextBox());
+			this->namingTextBox = (gcnew System::Windows::Forms::TextBox());
 			this->SuspendLayout();
 			// 
-			// button2
+			// exitBtn
 			// 
-			this->button2->BackColor = System::Drawing::Color::Black;
-			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-			this->button2->Font = (gcnew System::Drawing::Font(L"Segoe UI", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->exitBtn->BackColor = System::Drawing::Color::Black;
+			this->exitBtn->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->exitBtn->Font = (gcnew System::Drawing::Font(L"Segoe UI", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button2->ForeColor = System::Drawing::Color::Snow;
-			this->button2->Location = System::Drawing::Point(520, 311);
-			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(67, 29);
-			this->button2->TabIndex = 11;
-			this->button2->Text = L"Exit";
-			this->button2->UseVisualStyleBackColor = false;
-			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			this->exitBtn->ForeColor = System::Drawing::Color::Snow;
+			this->exitBtn->Location = System::Drawing::Point(548, 12);
+			this->exitBtn->Name = L"exitBtn";
+			this->exitBtn->Size = System::Drawing::Size(67, 29);
+			this->exitBtn->TabIndex = 11;
+			this->exitBtn->Text = L"Exit";
+			this->exitBtn->UseVisualStyleBackColor = false;
+			this->exitBtn->Click += gcnew System::EventHandler(this, &MyForm::exitBtn_Click);
 			// 
-			// button1
+			// createTxtFileBtn
 			// 
-			this->button1->BackColor = System::Drawing::Color::Black;
-			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-			this->button1->Font = (gcnew System::Drawing::Font(L"Segoe UI", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->createTxtFileBtn->BackColor = System::Drawing::Color::Black;
+			this->createTxtFileBtn->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->createTxtFileBtn->Font = (gcnew System::Drawing::Font(L"Segoe UI", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button1->ForeColor = System::Drawing::Color::Snow;
-			this->button1->Location = System::Drawing::Point(447, 311);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(67, 29);
-			this->button1->TabIndex = 12;
-			this->button1->Text = L"Minimise";
-			this->button1->UseVisualStyleBackColor = false;
+			this->createTxtFileBtn->ForeColor = System::Drawing::Color::Snow;
+			this->createTxtFileBtn->Location = System::Drawing::Point(12, 12);
+			this->createTxtFileBtn->Name = L"createTxtFileBtn";
+			this->createTxtFileBtn->Size = System::Drawing::Size(112, 29);
+			this->createTxtFileBtn->TabIndex = 12;
+			this->createTxtFileBtn->Text = L"Create Text File";
+			this->createTxtFileBtn->UseVisualStyleBackColor = false;
+			this->createTxtFileBtn->Click += gcnew System::EventHandler(this, &MyForm::createTxtFileBtn_Click_1);
+			// 
+			// inputTextBox
+			// 
+			this->inputTextBox->Location = System::Drawing::Point(12, 47);
+			this->inputTextBox->Name = L"inputTextBox";
+			this->inputTextBox->Size = System::Drawing::Size(218, 22);
+			this->inputTextBox->TabIndex = 13;
+			this->inputTextBox->Text = L"Input";
+			// 
+			// namingTextBox
+			// 
+			this->namingTextBox->Location = System::Drawing::Point(130, 17);
+			this->namingTextBox->Name = L"namingTextBox";
+			this->namingTextBox->Size = System::Drawing::Size(100, 22);
+			this->namingTextBox->TabIndex = 14;
+			this->namingTextBox->Text = L"Name ";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
-			this->ClientSize = System::Drawing::Size(599, 352);
-			this->Controls->Add(this->button1);
-			this->Controls->Add(this->button2);
+			this->ClientSize = System::Drawing::Size(627, 526);
+			this->Controls->Add(this->namingTextBox);
+			this->Controls->Add(this->inputTextBox);
+			this->Controls->Add(this->createTxtFileBtn);
+			this->Controls->Add(this->exitBtn);
 			this->DoubleBuffered = true;
 			this->Font = (gcnew System::Drawing::Font(L"Segoe UI", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
@@ -90,16 +122,32 @@ namespace ReadWriteTxtFiles {
 			this->MouseMove += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::MyForm_MouseMove);
 			this->MouseUp += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::MyForm_MouseUp);
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
 
-
-	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) 
+// Buttons section 
+//--------------------------------------------------------------------------------------------------------------------
+	private: System::Void exitBtn_Click(System::Object^ sender, System::EventArgs^ e) 
 	{
 		Application::Exit(); // When click exit button exit the application 
 	}
+	private: System::Void createTxtFileBtn_Click_1(System::Object^ sender, System::EventArgs^ e)
+	{
+		String^ fileTitle = namingTextBox->Text;
+		StreamWriter^ outFile = gcnew StreamWriter(fileTitle); // Designate the name of the file here, you can also 
+		  													   // give a specific path rather than a name for foreign files. 
+		String^ inputTxt = inputTextBox->Text;
+		outFile->Write(inputTxt);
 
+		outFile->Close(); 
+	}
+//--------------------------------------------------------------------------------------------------------------------
+
+
+// Dragging box around screen section 
+//--------------------------------------------------------------------------------------------------------------------
 		   bool dragging;
 		   Point offset;
 	private: System::Void MyForm_MouseDown(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e)
@@ -123,5 +171,6 @@ namespace ReadWriteTxtFiles {
 		// Disable dragging 
 		dragging = false;
 	}
-	};
+//--------------------------------------------------------------------------------------------------------------------
+};
 }
